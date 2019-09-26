2cdd1b7a886d26b3db782125a5283109f8db5d5b
unknown <brouwern@gmail.com>
2019-09-26 10:48:02 -0400

    'x'

--- inst/rebuild_package_script.R
+++ inst/rebuild_package_script.R
@@ -1,10 +1,18 @@
+# vignettes
 devtools::clean_vignettes()
 devtools::build_vignettes()
+
+# documents
 devtools::document(roclets=c('rd', 'collate', 'namespace'))
+
+
 devtools::load_all(".")
+
+# check
 devtools::check() # = "check package" on GUI, = R CMD check?
 
 
+# pkgdown
 pkgdown::build_site()
 
 
